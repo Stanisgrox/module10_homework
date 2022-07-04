0795900833405df7c0e94009d39b5d1481ac84f3
Stanislav <orlandonetworks@gmail.com>
2022-07-05 00:17:55 +0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,15 @@
+x = 123;
+
+tyx = typeof(x);
+
+if (tyx == "number"){
+    alert('Х - число');
+}
+
+if (tyx == "string"){
+    alert('Х - строка');
+}
+
+if (tyx == "boolean"){
+    alert('Х - логический');
+}
